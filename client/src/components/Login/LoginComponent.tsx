e227309e5d2e400b48b4ac2e6cad3d7488ca84cd
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-28 13:11:53 -0300

    validacao de formulario e informacoes de possiveis erros

--- client/src/components/Login/LoginComponent.tsx
+++ client/src/components/Login/LoginComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useContext, useState } from 'react'
 import { Container, Content, Form, Logo } from './styles'
 import { MdEmail } from 'react-icons/md'
 import { IoMdLock } from 'react-icons/io'
@@ -8,21 +8,42 @@ import axios from 'axios'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { AuthContext } from '@/context/userContext'
 
 /* separator */
 
 const schema = z.object({
-  email: z.coerce.string().email('Endereço de E-mail invalido'),
-  password: z.coerce.string().min(6, 'Senha deve conter 6 dígitos '),
+  email: z.coerce.string().email('Endereço de E-mail inválido'),
+  password: z.coerce.string().min(6, 'Senha deve conter 6 dígitos'),
 })
+
 type FormData = z.infer<typeof schema>
 
 /* separator */
-
+axios.defaults.baseURL = 'http://localhost:3333/user'
 export function LoginComponent() {
-  const navigate = useNavigate()
+  const { setAuthorized, setUserData } = useContext(AuthContext)
   const [animation, setAnimation] = useState(true)
-  axios.defaults.baseURL = 'http://localhost:3333/user'
+  const navigate = useNavigate()
+
+  const onSubmit = useCallback(
+    (data: FormData) => {
+      axios
+        .post('/login', data)
+        .then((response) => {
+          setAuthorized((prev) => !prev)
+          setAnimation(false)
+          setUserData(response.data.message)
+          setTimeout(() => {
+            navigate('/home')
+          }, 1000)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    },
+    [setAuthorized, setUserData, navigate],
+  )
 
   const {
     register,
@@ -32,34 +53,21 @@ export function LoginComponent() {
     resolver: zodResolver(schema),
   })
 
-  function onSubmit(data: FormData) {
-    axios
-      .post('/login', data)
-      .then(() => {
-        setAnimation(false)
-        setTimeout(() => {
-          navigate('/home')
-        }, 1000)
-      })
-      .catch(() => {
-        window.location.reload()
-      })
-  }
   return (
     <Container>
       <Content animation={animation}>
         <Form onSubmit={handleSubmit(onSubmit)}>
           <Logo Url_Logo={logo} />
-          <h1>ACESSE SUA CONTA</h1>
+          <h1>ACESSAR SUA CONTA</h1>
           <div>
-            <input type="text" placeholder="Email" {...register('email')} />
+            <input type="text" placeholder="E-mail" {...register('email')} />
             <span>{errors.email?.message}</span>
             <MdEmail size={18} />
           </div>
           <div>
             <input
               type="password"
-              placeholder="Password"
+              placeholder="Senha"
               {...register('password')}
             />
             <span>{errors.password?.message}</span>
