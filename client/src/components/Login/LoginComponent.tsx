c8615a9a967405063be134ebdf23bff20153a0e5
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-04-03 18:54:46 -0300

    adicionando card de status no componente de login

--- client/src/components/Login/LoginComponent.tsx
+++ client/src/components/Login/LoginComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useContext, useState } from 'react'
-import { Container, Content, Form, Logo } from './styles'
+import { Aside, Container, Content, Form, Logo } from './styles'
 import { MdEmail } from 'react-icons/md'
 import { IoMdLock } from 'react-icons/io'
 import logo from '@/img/logo.png'
@@ -9,6 +9,9 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { AuthContext } from '@/context/userContext'
+import { CardStatus } from '../CardStatus/CardStatus'
+import Success from '@/../public/success.svg'
+import Error from '@/../public/error.svg'
 
 /* separator */
 
@@ -25,21 +28,48 @@ export function LoginComponent() {
   const { setAuthorized, setUserData } = useContext(AuthContext)
   const [animation, setAnimation] = useState(true)
   const navigate = useNavigate()
-
+  const [visibilityCardStatus, setVisibilityCardStatus] =
+    useState<boolean>(false)
+  const [textCard, setTextCard] = useState('')
+  const [srcCard, setSrcCard] = useState('')
+  const [colorCard, setColorCard] = useState('')
+  const [statusCard, setStatusCard] = useState('')
+  const [fnCallbackCard, setFnCallbackCard] = useState<() => void>(() => {})
+  function itsSuccess() {
+    setVisibilityCardStatus(false)
+    navigate('/home')
+  }
+  function itsError() {
+    setVisibilityCardStatus((prev) => !prev)
+  }
   const onSubmit = useCallback(
     (data: FormData) => {
       axios
         .post('/login', data)
         .then((response) => {
           setAuthorized((prev) => !prev)
-          setAnimation(false)
           setUserData(response.data.message)
+          setTextCard(
+            `Login efetuado com sucesso, seja bem vindo ${response.data.message.name}.`,
+          )
+          setSrcCard(Success)
+          setStatusCard('SUCESSO')
+          setFnCallbackCard(() => itsSuccess)
+          setColorCard('green')
           setTimeout(() => {
-            navigate('/home')
-          }, 1000)
+            setVisibilityCardStatus(true)
+          }, 100)
+          setTimeout(() => {
+            setAnimation(false)
+          }, 100)
         })
         .catch((error) => {
-          console.error(error)
+          setVisibilityCardStatus((prev) => !prev)
+          setTextCard(`Algo deu errado. ${error.response.data.message}.`)
+          setSrcCard(Error)
+          setStatusCard('FALHA')
+          setFnCallbackCard(() => itsError)
+          setColorCard('red')
         })
     },
     [setAuthorized, setUserData, navigate],
@@ -76,6 +106,17 @@ export function LoginComponent() {
           <button type="submit">ENTRAR</button>
         </Form>
       </Content>
+      <Aside animation={visibilityCardStatus!}>
+        {visibilityCardStatus && (
+          <CardStatus
+            text={textCard}
+            src={srcCard}
+            status={statusCard}
+            fnCallback={fnCallbackCard!}
+            cor={colorCard}
+          />
+        )}
+      </Aside>
     </Container>
   )
 }
