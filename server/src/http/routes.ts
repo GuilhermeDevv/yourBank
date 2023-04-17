9661be6c9e8b6b87bbb142f81f69481b47bfcc81
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-04-17 17:38:56 -0300

    criacao da rota de atualizacao de dados

--- server/src/http/routes.ts
+++ server/src/http/routes.ts
@@ -3,4 +3,5 @@ import UserControllers from './controllers/UserControllers'
 export async function appRoutes(app: FastifyInstance) {
   app.get('/users/:email', UserControllers.getUser)
   app.post('/users', UserControllers.createUser)
+  app.put('/users', UserControllers.updateUser)
 }
