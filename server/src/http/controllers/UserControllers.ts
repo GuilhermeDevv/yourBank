4016c616998593f3d87233633b15780a89b2948b
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-04-18 17:07:32 -0300

    rota e suas funcoes para fazer as transicoes via 'pix' (mvp)

--- server/src/http/controllers/UserControllers.ts
+++ server/src/http/controllers/UserControllers.ts
@@ -5,14 +5,20 @@ import { PrismaUserRepository } from '@/repositories/prisma-repository'
 import { ListUserService } from '@/services/ListUserService/ListUserService'
 import { CreateUserServices } from '@/services/CreateUserServices/CreateUserServices'
 import { UpdateUserServices } from '@/services/UpdateUserServices/UpdateUserServer'
+import { TransactionUserServices } from '@/services/TransactionUserServices/TransactionUserServices'
 
+interface TransactionData {
+  senderUserEmail: string
+  receiverUserEmail: string
+  amount: string
+}
 class UserController {
   async getUser(request: FastifyRequest, reply: FastifyReply) {
     const { email } = <User>request.params
     const repository = new PrismaUserRepository()
     const listUserService = new ListUserService(repository)
     const { message, status } = await listUserService.execute(email)
-    reply.status(status).send(message)
+    reply.status(status).send({ message })
   }
 
   async createUser(request: FastifyRequest, reply: FastifyReply) {
@@ -20,7 +26,7 @@ class UserController {
     const repository = new PrismaUserRepository()
     const createUser = new CreateUserServices(repository)
     const { message, status } = await createUser.execute(data)
-    return reply.send(message).status(status)
+    return reply.status(status).send({ message })
   }
 
   async updateUser(request: FastifyRequest, reply: FastifyReply) {
@@ -28,8 +34,22 @@ class UserController {
     const repository = new PrismaUserRepository()
     const updateUserServices = new UpdateUserServices(repository)
     const { message, status } = await updateUserServices.execute(email, data)
-    return reply.status(status).send(message)
+    return reply.status(status).send({ message })
+  }
+
+  async transactionPix(request: FastifyRequest, reply: FastifyReply) {
+    const { senderUserEmail, receiverUserEmail, amount } = <TransactionData>(
+      request.body
+    )
+    const repository = new PrismaUserRepository()
+    const transactionPix = new TransactionUserServices(repository)
+    const { message, status } = await transactionPix.execute(
+      senderUserEmail,
+      receiverUserEmail,
+      amount,
+    )
+
+    return reply.status(status).send({ message })
   }
 }
-
 export default new UserController()
--- server/src/http/routes.ts
+++ server/src/http/routes.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import UserControllers from './controllers/UserControllers'
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/users/:email', UserControllers.getUser)
-  app.post('/users', UserControllers.createUser)
-  app.put('/users', UserControllers.updateUser)
+  app.get('/user/:email', UserControllers.getUser)
+  app.post('/user', UserControllers.createUser)
+  app.put('/user', UserControllers.updateUser)
+  app.post('/user/transactions', UserControllers.transactionPix)
 }
