19eaf928cd4a0bc87323381c018b6cb4a0154a4a
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-04-21 13:24:56 -0300

    criando a interface das transacoes de $ dos repositorios

--- server/src/repositories/IUserRepository.ts
+++ server/src/repositories/IUserRepository.ts
@@ -3,5 +3,16 @@ interface IUserRepository {
   findByEmail(email: string): Promise<User | null>
   create(data: { name: string; password: string; email: string }): Promise<void>
   update(data: Partial<User>, email: string): Promise<boolean>
+  transaction(data: { sender: User; receiver: User; amount: number }): Promise<{
+    id: number
+    senderId: string
+    receiverId: string
+    amount: number
+  }>
+  transactionLog(data: {
+    message: string
+    transactionId: number
+    status: string
+  }): Promise<boolean>
 }
 export default IUserRepository
