821a041da354a2791481e24833cd2d5cc2c3c3ab
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-04-24 13:57:51 -0300

    verificando acesso do usuario

--- server/src/services/ListUserService/ListUserService.ts
+++ server/src/services/ListUserService/ListUserService.ts
@@ -1,12 +1,16 @@
 import IUserRepository from '@/repositories/IUserRepository'
+import { User } from '@prisma/client'
 
 export class ListUserService {
   constructor(private repository: IUserRepository) {}
-  async execute(email: string) {
-    const response = await this.repository.findByEmail(email)
+  async execute(data: User) {
+    const response = await this.repository.findByEmail(data.email)
     if (response === null) {
       return { message: 'erro, usuário nao encontrado.', status: 404 }
     }
-    return { message: response, status: 200 }
+    if (response.email === data.email && response.password === data.password) {
+      return { message: '', status: 204 }
+    }
+    return { message: 'Senha invalida!', status: 401 }
   }
 }
