b1d77a9c8ca495d13fa718b37fceffc28b92b92b
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-04-30 14:54:11 -0300

    colocando o provedor de dados para toda a aplicacao

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import AuthContextProvider from './context/userContext'
 
 // eslint-disable-next-line no-undef
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
