a120aa58d6e280bf4a61f78dc5c42057dfb71b6f
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-05-13 19:00:06 -0300

    enviando o password na resposta do service e ajustando a mensagem de retorno

--- server/src/services/ListUserService/ListUserService.ts
+++ server/src/services/ListUserService/ListUserService.ts
@@ -6,20 +6,27 @@ export class ListUserService {
   async execute(data: User) {
     const response = await this.repository.findByEmail(data.email)
     if (response === null) {
-      return { message: 'erro, usuário nao encontrado.', status: 404 }
+      return { message: 'Usuário nao encontrado', status: 404 }
     }
     if (response.email === data.email && response.password === data.password) {
-      const { email, name, balance, receivedTransactions, sentTransactions } =
-        response
-      const userData = {
+      const {
         email,
         name,
         balance,
         receivedTransactions,
         sentTransactions,
+        password,
+      } = response
+      const userData = {
+        email,
+        name,
+        balance,
+        password,
+        receivedTransactions,
+        sentTransactions,
       }
       return { message: userData, status: 200 }
     }
-    return { message: 'Senha invalida!', status: 401 }
+    return { message: 'Senha invalida', status: 401 }
   }
 }
