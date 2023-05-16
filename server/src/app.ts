1237f1d9c1bcc265bb7920894978d8eaf788b8f4
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-05-16 18:32:09 -0300

    ajuste in cors of application

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,5 +3,7 @@ import { appRoutes } from './http/routes'
 import cors from '@fastify/cors'
 
 export const app = fastify()
-app.register(cors)
+app.register(cors, {
+  origin: 'https://your-bank-site.vercel.app',
+})
 app.register(appRoutes)
