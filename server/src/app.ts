dc29f15b415208465e78bfcd5d642d72101c19b9
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-05-17 17:11:53 -0300

    possible ajuste of error

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,5 +3,10 @@ import { appRoutes } from './http/routes'
 import cors from '@fastify/cors'
 
 export const app = fastify()
-app.register(cors)
+app.register(cors, {
+  origin: true,
+  credentials: true,
+  optionsSuccessStatus: 204,
+  methods: ['POST', 'PUT'],
+})
 app.register(appRoutes)
