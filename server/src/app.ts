23901f6ef86875aeb57417327f3cb2c1d5849680
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-05-17 17:44:22 -0300

    ajuste for request

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,10 +3,5 @@ import { appRoutes } from './http/routes'
 import cors from '@fastify/cors'
 
 export const app = fastify({ requestTimeout: 5000 })
-app.register(cors, {
-  origin: true,
-  credentials: true,
-  optionsSuccessStatus: 204,
-  methods: ['POST', 'PUT'],
-})
+app.register(cors)
 app.register(appRoutes)
