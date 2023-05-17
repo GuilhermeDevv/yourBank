92514b7f300c05e0c298b6dd5de9eb31d8c20458
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-05-17 17:45:08 -0300

    set requestTimeOut for 10 seconds

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,6 @@ import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import cors from '@fastify/cors'
 
-export const app = fastify({ requestTimeout: 5000 })
+export const app = fastify({ requestTimeout: 10000 })
 app.register(cors)
 app.register(appRoutes)
